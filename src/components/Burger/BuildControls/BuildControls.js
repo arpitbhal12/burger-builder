acf4db090e6d2e12b9ad251710de61195ba31ac5
arpitbhal12 <arpit12bhal@gmail.com>
2020-12-17 23:33:13 +0530

    Added starter code

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import BuildControl from './BuildControl/BuildControl'
+import classes from './BuildControls.module.css'
+
+const BuildControls = (props) => {
+    const controls = [
+        {label: 'salad', type: 'salad'},
+        {label: 'cheese', type: 'cheese'},
+        {label: 'bacon', type: 'bacon'},
+        {label: 'meat', type: 'meat'}
+    ]
+    return (
+        <div className={classes.BuildControls}>
+            {controls.map((index) => {
+                return <BuildControl key={index.label} type={index.label} />
+            })}
+        </div>
+    )
+}
+
+export default BuildControls;
