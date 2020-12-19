3c4d917d91793e498895c481b597d294b27fdd10
arpitbhal12 <arpit12bhal@gmail.com>
2020-12-19 23:31:08 +0530

    Side-drawer and backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import Aux from '../../hoc/Aux'
-import classes from './Layout.module.css'
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux';
+import classes from './Layout.module.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const Layout = (props) => {
-    
-	return (
-		<Aux>
-			<Toolbar />
-			<SideDrawer />
-			<main className={classes.Content}>{props.children}</main>
-		</Aux>
-	);
-};
+
+class Layout extends Component {
+	state = {
+		showSideDrawer: true
+	}
+
+	showSideDrawerHandler = () => {
+		this.setState({showSideDrawer: false});
+	}
+	render() {
+		return (
+			<Aux>
+				<Toolbar />
+				<SideDrawer show={this.state.showSideDrawer} clicked={this.showSideDrawerHandler}/>
+				<main className={classes.Content}>{this.props.children}</main>
+			</Aux>
+		);
+	}
+}
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -2,15 +2,22 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
 import classes from './SideDrawer.module.css';
+import Aux from '../../../hoc/Aux';
+import BackDrop from '../../UI/BackDrop/BackDrop';
 
 const SideDrawer = (props) => {
 	return (
-		<div className={classes.SideDrawer}>
-			<Logo />
-			<nav>
-				<NavigationItems />
-			</nav>
-		</div>
+		<Aux>
+			<BackDrop show={props.show} hideSummary={props.clicked}/>
+			<div className={classes.SideDrawer}>
+				<div className={classes.Logo}>
+					<Logo />
+				</div>
+				<nav>
+					<NavigationItems />
+				</nav>
+			</div>
+		</Aux>
 	);
 };
 
