a1bc768265482dcb20e8dc8facd5ca719159d350
arpitbhal12 <arpit12bhal@gmail.com>
2020-12-23 21:30:34 +0530

    Added error handler

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -6,6 +6,7 @@ import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 import Axios from './../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import WithErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 
 const INGREDIENTS_PRICE = {
 	salad: 0.1,
@@ -127,4 +128,4 @@ class BurgerBuilder extends Component {
 	}
 }
 
-export default BurgerBuilder;
+export default WithErrorHandler(BurgerBuilder, Axios);
---
+++ src/hoc/WithErrorHandler/WithErrorHandler.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Aux from '../Aux/Aux';
+import Modal from '../../components/UI/Modal/Modal';
+
+const WithErrorHandler = (WrappedComponent, axios) => {
+	return class extends Component {
+        state = {
+            error: false
+        }
+
+        componentDidMount() {
+            axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            });
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            })
+        }
+
+        errorClear = () => {
+            this.setState({error: false});
+        }
+		render() {
+            console.log("dsfsf");
+			return (
+				<Aux>
+					<Modal show={this.state.error} modalClosed={this.errorClear}>
+						<p>Error</p>
+					</Modal>
+					<WrappedComponent {...this.props} />
+				</Aux>
+			);
+		}
+	};
+};
+
+export default WithErrorHandler;
